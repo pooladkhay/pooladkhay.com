94b723704856d9c106e48df015e5aa5a30a76a77
MJ Pooladkhay <mj@pooladkhay.com>
2025-11-08 16:10:53 +0000

    add wrangler and build script
    Signed-off-by: MJ Pooladkhay <mj@pooladkhay.com>

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ZOLA_VERSION='0.21.0'
+
+echo "Installing Zola ${ZOLA_VERSION}..."
+
+# Download
+curl -sL -o "zola.tar.gz" "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+
+# Extract
+mkdir -p "${HOME}/.local/bin"
+tar -xzf zola.tar.gz -C "${HOME}/.local/bin"
+rm zola.tar.gz
+
+export PATH="${HOME}/.local/bin:${PATH}"
+
+echo "Building the site..."
+zola build -f
